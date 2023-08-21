a2be99eaef913058fa88126a40952a3be3292b35
etohRP <22016439@myrp.edu.sg>
2023-08-21 13:23:18 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22016439/mywebbrowser/MainActivity.java
@@ -0,0 +1,36 @@
+package sg.edu.rp.c346.id22016439.mywebbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoad;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wvMyPage = findViewById(R.id.wvMyPage);
+        btnLoad = findViewById(R.id.btnLoad);
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.rp.edu.sg/soi/home";
+                wvMyPage.loadUrl(url);
+            }
+        });
+
+    }
+
+
+}
